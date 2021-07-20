98f5e738e1dad37b332af19b37df75b8a7a9d69d
miftahuljannah4 <Monicautami4@gmail.com>
2021-07-21 00:38:45 +0700

    uas

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,71 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Bootstrap Navbar Sidebar - Fixed to Left or Right</title>
-    <link rel="stylesheet"
-          id="theme_link"
-          href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css"/>
-    <link rel="stylesheet" href="navbar-fixed-right.min.css">
-    <link rel="stylesheet" href="navbar-fixed-left.min.css">
-    <link rel="stylesheet" href="docs.css">
-    <script
-            src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
-            crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-            crossorigin="anonymous"></script>
-    <script src="docs.js"></script>
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Loket</title>
+        <link
+            rel="stylesheet"
+            id="theme_link"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css"/>
+        <link rel="stylesheet" href="navbar-fixed-right.min.css">
+        <link rel="stylesheet" href="navbar-fixed-left.min.css">
+        <link rel="stylesheet" href="docs.css">
+        <script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"
+></script>
+<!-- Font Awesome -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+  rel="stylesheet"
+/>
+<!-- Google Fonts -->
+<link
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+  rel="stylesheet"
+/>
+<!-- MDB -->
+<link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css"
+  rel="stylesheet"
+/>
+    </head>
 
-<body>
-<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
-    <a class="navbar-brand" href>Menu</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a href="<?php echo base_url('pelayanan'); ?>" class="nav-link">Pelayananan</a>
-            </li>
-            <!-- <li class="nav-item">
-                <a href="<?php echo base_url('pelayanan'); ?>" class="nav-link">Loket</a>
-            </li> -->
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Antrian</a>
-                <div class="dropdown-menu">
-                    <a href="<?php echo base_url('antrian'); ?>" class="dropdown-item">Ambil Antrian</a>
-                    <a href="<?php echo base_url('antrianno'); ?>" class="dropdown-item">No Antrian</a>
-                    <a href="<?php echo base_url('loketpanggil'); ?>" class="dropdown-item">Loket Antrian</a>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="<?php echo base_url('pelayanan'); ?>">pelayanan</a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a
+                            class="nav-link active"
+                            aria-current="page"
+                            href="<?php echo base_url('antrian'); ?>">Ambil Antrian</a>
+                        <a class="nav-link" href="<?php echo base_url('antrianno'); ?>">Nomor Antrian
+                        </a>
+                        <a class="nav-link" href="<?php echo base_url('loketpanggil'); ?>">Loket Antrian</a>
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a >
+                    </div>
                 </div>
-            </li>
-        </ul>
-        
-    </div>
-</nav>
+            </div>
+        </nav>
 
-<div class="container">
-    <div class="jumbotron">
-        <h1 class="display-4">
-            Aplikasi antrian berbasis WEB dengan Framework CodeIgniter 4
-            <br>
-        </h1>
-        <p>
-            <strong>
-            </strong>
-        </p>
-        
-    </div>
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4"><b>WELCOME </h1>
+                <h2>
+                    Antrian Loket dengan FrameWork Codeigniter 4
+                    Tugas Ujian Akhir Semester 4 Pemograman Web
+                    <h2>
+              
 
-    
+            </div>
 
-</div>
+        </div>
 
-</body>
+    </body>
 </html>
